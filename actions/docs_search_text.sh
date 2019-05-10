c64b8ceea2800a5ffff603c9f1be04fb9fca2155
demonlibra <demonlibra@inbox.ru>
2019-05-10 11:31:03 +0300

    docs_search_text. Add choise temperory or constantly to keep index files

--- actions/docs_search_text.sh
+++ actions/docs_search_text.sh
@@ -2,17 +2,21 @@
 
 #Дополнительные параметры
 
-fullpathname=$@
-name=${fullpathname##*/}
-path=${fullpathname%/*}
+temp_index="/tmp"				#Каталог для временного индекса. Хранится только до перезагрузки ПК.
+home_index="$HOME/.recoll"		#Каталог для постоянного индекса
 
-PathIndex="/tmp"
-FORM=`yad --borders=10 --title="Поиск строк в докуметах - recoll" --form --item-separator="|" --separator="," --field=":LBL" --field="Введите строку для поиска" --field="Вывод:CB" "" "" "zenity|^recoll"`
+FORM=`yad --borders=10 --title="Поиск строк в докуметах - recoll" --form --item-separator="|" --separator="," --field=":LBL" --field="Введите строку для поиска" --field="Вывод результата:CB" --field="Сохранить индекс после перезагрузки ПК:CB" "" "" "zenity|^recoll" "^Да|Нет"`
 
 if [ $? = 0 ]
 	then
 		text=$( echo $FORM | awk -F ',' '{print $2}')
 		out=$( echo $FORM | awk -F ',' '{print $3}')
+		save_index=$( echo $FORM | awk -F ',' '{print $4}')
+		
+		if [[ "$save_index" = "Да" ]]
+			then PathIndex=$home_index
+			else PathIndex=$temp_index
+		fi
 		
 		find "$@" -type f \( -iname "*.pdf" -or -iname "*.doc" -or -iname "*.docx" -or -iname "*.xls" -or -iname "*.xlsx" -or -iname "*.txt" -or -iname "*.rtf" -or -iname "*.odt" \) -print | recollindex -c $PathIndex -i -e -f
 				
