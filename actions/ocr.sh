0b121513f2268388c091e2fdb13b9a8b9a0b2995
demonlibra <demonlibra@inbox.ru>
2020-05-03 12:05:56 +0300

    OCR - Added multiple file selection

--- actions/ocr.sh
+++ actions/ocr.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-fullpathname=$@
-name=${fullpathname##*/}
-path=${fullpathname%/*}
-
-FORM=`yad --borders=10 --width=300 --title="Распознавание изображения" --text-align=center --text="Укажите параметры" --form --item-separator="|" --separator="," --field=:LBL --field="Выберите программу распознавания:CB" --field="Выберите язык текста:CB" "" "cuneiform|^tesseract" "Русский|Английский|Итальянский|Смешанный"`
+FORM=`yad --borders=10 --width=300 --title="Распознавание изображения" --text-align=center --text="Укажите параметры" --form --item-separator="|" --separator="," --field=:LBL --field="Выберите программу распознавания:CB" --field="Выберите язык текста:CB" "" "cuneiform|^tesseract" "Русский|Английский|^Итальянский|Смешанный"`
 
 if [ $? = 0 ]
 	then
 	
 	ocr=$( echo $FORM | awk -F ',' '{print $2}')
-	lang=$( echo $FORM | awk -F ',' '{print $3}')
+	language=$( echo $FORM | awk -F ',' '{print $3}')
+
+	(for file in "$@"
+		do
+			if [ $ocr = "cuneiform" ]
+				then
+					if [ "$language" = "Русский" ]; then lang="rus"
+					elif [ "$language" = "Английский" ]; then lang="eng"
+					elif [ "$language" = "Итальянский" ]; then lang="ita"
+					else language="ruseng"
+					fi
+
+					cuneiform -f text -l $lang -o "${file%.*}-OCR_cuneiform_$lang.txt" "$file"
+
+			elif [ $ocr = "tesseract" ]
+				then
+					if [ "$language" = "Русский" ]; then lang="rus"
+					elif [ "$language" = "Английский" ]; then lang="eng"
+					elif [ "$language" = "Итальянский" ]; then lang="ita"
+					else language="rus+eng"
+					fi
+
+					tesseract "$file" "${file%.*}-OCR_tesseract_$lang" -l $lang
+			fi
+		done)|zenity --progress --title="Распознавание текста" --auto-close --auto-kill --width=250
 	
-	if [ $ocr = "cuneiform" ]
-		then
-			if [ "$lang" = "Русский" ]; then lang="rus"
-			elif [ "$lang" = "Английский" ]; then lang="eng"
-			else lang="ruseng"
-			fi
-			
-			cuneiform -f text -l $lang -o "${fullpathname%.*}-OCR_cuneiform_$lang.txt" "$fullpathname"
-			
-	elif [ $ocr = "tesseract" ]
-		then
-			if [ "$lang" = "Русский" ]; then lang="rus"
-			elif [ "$lang" = "Английский" ]; then lang="eng"
-			elif [ "$lang" = "Итальянский" ]; then lang="ita"
-			else lang="rus+eng"
-			fi
-			
-			tesseract "$fullpathname" "${fullpathname%.*}-OCR_tesseract_$lang" -l $lang
-	fi
-	notify-send -t 10000 -i "gtk-ok" "$ocr" "Завершено распознавание текста в изображении $name"
+	notify-send -t 10000 -i "gtk-ok" "$ocr" "Завершено распознавание текста в изображениях"
 fi
 
