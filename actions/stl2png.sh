9fdec22d0ae2c7d58f5071c0973be9512f1e4564
demonlibra <demonlibra@inbox.ru>
2020-11-28 00:18:35 +0300

    Action stl2png - added projections and camera options

--- actions/stl2png.sh
+++ actions/stl2png.sh
@@ -10,7 +10,7 @@ if [ -z "`dpkg -l | grep openscad`" ]
 	then gnome-terminal --hide-menubar --geometry=80x15 -t "Установка пакета openscad" -- bash -c "echo \"openscad не установлен\"; echo ; sudo apt install openscad; echo ; echo ------------------ ; echo ; echo \"Установка openscad завершена\"; echo ; read -p \"Нажмите ENTER чтобы закрыть окно\""
 fi
 
-AAA=`yad --borders=10 --title="stl 2 png" --text="Конвертация stl в png" --text-align=center --form --field=:LBL --field="Ширина изображения" --field="Высота изображения" --field="Автоцентровка:CHK" --field="Вписать модель в размер:CHK" "" 1080 1080 TRUE TRUE`
+AAA=`yad --borders=10 --title="stl 2 png" --text="Конвертация stl в png" --text-align=center --form --item-separator="|" --field=:LBL --field="Ширина изображения" --field="Высота изображения" --field="Автоцентровка:CHK" --field="Вписать модель в размер:CHK" --field="Проекция:CB" --field="Камера авто:CHK" --field="Камера ручн." "" 1080 1080 TRUE TRUE "ortho|perspective" TRUE "0,0,0,25,0,35,500"`
 
 if [ $? = 0 ]
 	then
@@ -22,14 +22,20 @@ if [ $? = 0 ]
 		
 		viewall=$( echo $AAA | awk -F '|' '{print $5}')
 		if [ $viewall = "TRUE" ]; then viewall='--viewall'; else viewall=''; fi
-		
+
+                projection=$( echo $AAA | awk -F '|' '{print $6}')
+
+                camera_auto=$( echo $AAA | awk -F '|' '{print $7}')
+                camera_man=$( echo $AAA | awk -F '|' '{print $8}')
+                if [ "$camera_auto" != "TRUE" ]; then camera='--camera='${camera_man}; else camera=''; fi
+
 		for file in "$@"
 			do
 				#Создание временного файла scad с командой импорта stl
 				echo "import(\"${file}\");" > "${file%.*}.scad"
 
 				# Экспорт в png при помощи openSCAD
-				openscad -o "${file%.*}.png" $autocenter $viewall --imgsize=$width,$height "${file%.*}.scad"
+				openscad -o "${file%.*}.png" $camera --projection=$projection $autocenter $viewall --imgsize=$width,$height "${file%.*}.scad"
 
 				# Удаление временного файла
 				rm "${file%.*}.scad"
