f94845a832651bf3d9f00eb9a342622e39a6f7af
demonlibra <demonlibra@inbox.ru>
2020-11-28 08:48:25 +0300

    Action stl2png - Some fixes

--- actions/stl2png.sh
+++ actions/stl2png.sh
@@ -10,20 +10,22 @@ if [ -z "`dpkg -l | grep openscad`" ]
 	then gnome-terminal --hide-menubar --geometry=80x15 -t "Установка пакета openscad" -- bash -c "echo \"openscad не установлен\"; echo ; sudo apt install openscad; echo ; echo ------------------ ; echo ; echo \"Установка openscad завершена\"; echo ; read -p \"Нажмите ENTER чтобы закрыть окно\""
 fi
 
-AAA=`yad --borders=10 --title="stl 2 png" --text="Конвертация stl в png" --text-align=center --form --item-separator="|" --field=:LBL --field="Ширина изображения" --field="Высота изображения" --field="Автоцентровка:CHK" --field="Вписать модель в размер:CHK" --field="Проекция:CB" --field="Камера авто:CHK" --field="Камера ручн." "" 1080 1080 TRUE TRUE "ortho|perspective" TRUE "0,0,0,25,0,35,500"`
+AAA=`yad --borders=10 --title="stl 2 png" --text="Конвертация stl в png" --text-align=center --form --item-separator="|" --field=:LBL --field="Ширина изображения" --field="Высота изображения" --field="Проекция:CB" --field="Автоцентровка:CHK" --field="Вписать модель в размер:CHK" --field="Камера авто:CHK" --field="Камера ручн." --field="              transx,transy,transz,rotx,roty,rotz,distance:LBL" "" 1080 1080  "ortho|perspective" TRUE TRUE TRUE "0,0,0,25,0,35,500"`
 
 if [ $? = 0 ]
 	then
 		width=$( echo $AAA | awk -F '|' '{print $2}')
 		height=$( echo $AAA | awk -F '|' '{print $3}')
-		
-		autocenter=$( echo $AAA | awk -F '|' '{print $4}')
+
+                projection=$( echo $AAA | awk -F '|' '{print $4}')
+
+		autocenter=$( echo $AAA | awk -F '|' '{print $5}')
 		if [ $autocenter = "TRUE" ]; then autocenter='--autocenter'; else autocenter=''; fi
 		
-		viewall=$( echo $AAA | awk -F '|' '{print $5}')
+		viewall=$( echo $AAA | awk -F '|' '{print $6}')
 		if [ $viewall = "TRUE" ]; then viewall='--viewall'; else viewall=''; fi
 
-                projection=$( echo $AAA | awk -F '|' '{print $6}')
+
 
                 camera_auto=$( echo $AAA | awk -F '|' '{print $7}')
                 camera_man=$( echo $AAA | awk -F '|' '{print $8}')
