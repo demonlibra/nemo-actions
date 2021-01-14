934dcb0efe3d500959e63ebf9a864032b33b04c5
demonlibra <demonlibra@inbox.ru>
2021-01-14 23:34:21 +0500

    Action dwgdxf_convert.sh - Added styles

--- actions/dwgdxf_convert.sh
+++ actions/dwgdxf_convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Дополнительные параметры
-pathtoqcad="$HOME/App/qcad" #Путь к папке, содержащей скрипты dwg2bmp, dwg2svg и dwg2pdf
+pathtoqcad="$HOME/app/qcad" #Путь к папке, содержащей скрипты dwg2bmp, dwg2svg и dwg2pdf
 
 #Проверка наличия программы QCAD
 if ! [ -f "$pathtoqcad/dwg2bmp" ] && ! [ -f "$pathtoqcad/dwg2svg" ] && ! [ -f "$pathtoqcad/dwg2pdf" ]
@@ -15,7 +15,9 @@ name=${fullpathname##*/}
 path=${fullpathname%/*}
 ext=${fullpathname##*.}
 
-AAA=`yad --borders=10 --width=300 --title="QCAD" --text="Преобразовать $name в изображение" --text-align=center --form --item-separator="|" --separator="," --field=":LBL" --field="Введите ширину:NUM" --field="Введите высоту:NUM" --field="Формат:CB" "" "2000" "1000" "bmp|jpg|^png|pdf|ppm|svg|tiff"`
+AAA=`yad --borders=10 --width=300 --title="QCAD" --text="Преобразовать $name в изображение" --text-align=center --form --item-separator="|" --separator="," \
+--field=":LBL" --field="Введите ширину:NUM" --field="Введите высоту:NUM" 	  	    --field="Формат:CB"				 --field="Стиль:CB" \
+     ""							"2000"			  				   "1000"				  "bmp|jpg|^png|pdf|ppm|svg|tiff"	 "|grayscale|monochrome"`
 
 if [ $? = 0 ] 
 	then
@@ -23,16 +25,24 @@ if [ $? = 0 ]
 		height=$( echo $AAA | awk -F ',' '{print $3}')
 		format=$( echo $AAA | awk -F ',' '{print $4}')
 		
+		style=$( echo $AAA | awk -F ',' '{print $5}')
+		if [ $style = "grayscale" ]
+			then style_option="-k"
+		elif [ $style = "monochrome" ]
+			then style_option="-n"
+			else style_option=""
+		fi
+		
 		if [ $format = "svg" ]
 			then #gnome-terminal -t "dwg2svg \"$name\"" -e "sh $pathtoqcad/dwg2svg -f \"$fullpathname\""
-					bash "$pathtoqcad/dwg2svg" -f "$fullpathname"
+					bash "$pathtoqcad/dwg2svg" -f -o "$fullpathname"
 
 			elif [ $format = "pdf" ]
 				then #gnome-terminal -t "dwg2pdf \"$name\"" -e "sh $pathtoqcad/dwg2pdf -f -a -p \"$width\"x\"$height\" \"$fullpathname\""
-						bash "$pathtoqcad/dwg2pdf" -f -a -p "$width"x"$height" "$fullpathname"
+						bash "$pathtoqcad/dwg2pdf" -f -a $style_option -p "$width"x"$height" -o "${fullpathname%.*}_$style.pdf" "$fullpathname"
 
 			else #gnome-terminal -t "dwg2bmp \"$name\"" -e "sh $pathtoqcad/dwg2bmp -f -b white -x $width -y $height -o \"${fullpathname%.*}.$format\" \"$fullpathname\""
-					bash "$pathtoqcad/dwg2bmp" -f -b white -x $width -y $height -o "${fullpathname%.*}.$format" "$fullpathname"
+					bash "$pathtoqcad/dwg2bmp" -f -b white $style_option -x $width -y $height -o "${fullpathname%.*}_$style.$format" "$fullpathname"
 		fi
 
 	notify-send -t 10000 -i "gtk-ok" "Завершено" "Преобразование файла $name в формат $format"
