967e6726d26997fa0f6a42442515b43d4e7435d5
demonlibra <demonlibra@inbox.ru>
2021-04-11 23:23:17 +0500

    Git add - some fixes

--- actions/git.sh
+++ actions/git.sh
@@ -21,8 +21,9 @@ fi
 
 # Добавление файлов для загрузки
 echo
-#git add .
-echo "Выполнена команда: git add ."
+git add .
+echo -e '\E[1;32m'"Выполнена команда: git add ."
+tput sgr0 
 
 # Ввод коммита
 echo
@@ -30,12 +31,16 @@ echo -n "Введите текст коммита: "
 read commit_text
 
 echo
-#git commit -m "${commit_text}"
-echo "Выполнена команда: git commit -m \"${commit_text}\""
+git commit -m "${commit_text}"
+echo
+echo -e '\E[1;32m'"Выполнена команда: git commit -m \"${commit_text}\""
+tput sgr0 
 
 echo
-#git push
-echo "Выполнена команда: git push"
+git push
+echo
+echo -e '\E[1;32m'"Выполнена команда: git push"
+tput sgr0 
 
 echo
 read -p "Нажмите ENTER чтобы закрыть окно"
