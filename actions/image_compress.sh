cc37faf7ba3f0dd26301b72dc5bcec2b16e5c626
demonlibra <demonlibra@inbox.ru>
2022-07-04 11:05:34 +0300

    image_compress Added more options

--- actions/image_compress.sh
+++ actions/image_compress.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
-fullpathname=$@
-name=${fullpathname##*/}
-path=${fullpathname%/*}
+path=$1
+path=${path%/*}
 
-if [[ -n `echo $fullpathname | grep "//"` ]]
+if [[ -n `echo $path | grep "//"` ]]
 	then
 		notify-send -t 10000 -i "error" "Остановлено" "Путь содержит некорректные символы. \nВозможно повреждение файлов!!!\n\nСкопируйте файлы на локальный носитель."
-		notify-send -t 10000 -i "error" "Остановлено" "$fullpathname"
+		notify-send -t 10000 -i "error" "Остановлено" "$path"
 		exit
 fi
 
-AAA=`yad --borders=10 --width=300 --title="Сжатие изображений" --text="Процент сжатия" --scale --value=85 --max-value=100 --step=1 --inc-buttons`
+parameters=`yad --borders=20 --width=500 --title="Сжатие изображений" \
+	--text-align=center --item-separator="|" --form \
+	--field="Процент сжатия:SCL"	--field=" :LBL"		--field="Каталог для сохранения:DIR"	--field="Добавить суфикс к имени файла:CHK"\
+				"85"						""						"$path"									"FALSE"			`
+exit_status=$?
 
-if [ $? = 0 ]
+if [ $exit_status = 0 ]
 	then
-		kolfile=$#					#Количество выделенных файлов
-		procent=$((100/$kolfile))	#Процентов на каждый файл
+		quality=$( echo $parameters | awk -F '|' '{print $1}')
+		dir=$( echo $parameters | awk -F '|' '{print $3}')
+		sufix=$( echo $parameters | awk -F '|' '{print $4}')
+
+		kolfile=$#														#Количество выделенных файлов
+		procent=$((100/$kolfile))										#Процентов на каждый файл
 
 		(for file in "$@"
 			do
-				convert "$file" -quality $AAA "$file"
+				filename="${file##*/}"
+				if [ $sufix == TRUE ]
+					then file_out="$dir/${filename%.*}_$quality.${file##*.}"
+					else file_out="$dir/$filename"
+				fi
+
+				convert "$file" -quality $quality "$file_out"
+
 				counter=$(($counter+1))
 				progress=$(($progress+$procent))
 				echo $progress
