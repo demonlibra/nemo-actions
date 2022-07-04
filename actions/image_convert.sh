945937e2c92b2300671fbc1d0f6bd1c8a221522a
demonlibra <demonlibra@inbox.ru>
2022-07-04 11:36:32 +0300

    image_convert added dir

--- actions/image_convert.sh
+++ actions/image_convert.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-fullpathname=$@
-name=${fullpathname##*/}
-path=${fullpathname%/*}
+path=$1
+path=${path%/*}
 
-AAA=`zenity --list --radiolist --title="Конвертирование изображений" --text="Выберите формат" --column="Выбор" --column="Формат" TRUE jpg FALSE png FALSE bmp FALSE tiff FALSE gif FALSE pdf --ok-label="Конвертировать" --cancel-label="Отмена" --width=300 --height=240`
+parameters=`yad --borders=20 --width=500 --title="Конвертирование изображений" \
+	--text-align=center --item-separator="|" --form \
+	--field="Формат:CB"				--field="Каталог для сохранения:DIR" \
+	"^jpg|png|bmp|tiff|gif|pdf"					"$path"`
 
-if [ $? = 0 ]
+exit_status=$?
+
+if [ $exit_status = 0 ]
 	then
+
+		format=$( echo $parameters | awk -F '|' '{print $1}')
+		dir=$( echo $parameters | awk -F '|' '{print $2}')
+		
 		kolfile=$#					#Количество выделенных файлов
 		procent=$((100/$kolfile))	#Процентов на каждый файл
 		
 		(for file in "$@"
 			do
-				mogrify -format $AAA "$file"
+				mogrify -format $format -path "$dir" "$file"
 				counter=$(($counter+1))
 				progress=$(($progress+$procent))
 				echo $progress
 				echo "# Обработано $counter из $kolfile"
-			done)|zenity --progress --title="Конвертирование изображений в формат $AAA" --auto-close --auto-kill --width=350
+			done)|zenity --progress --title="Конвертирование изображений в формат $format" --auto-close --auto-kill --width=350
 
-		notify-send -t 10000 -i "gtk-ok" "Завершено" "Конвертирование изображений в формат $AAA"
+		notify-send -t 10000 -i "gtk-ok" "Завершено" "Конвертирование изображений в формат $format"
 fi
