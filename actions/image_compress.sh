82b483699b9774fcec8717f14b0002e3bd91948d
demonlibra <demonlibra@inbox.ru>
2022-07-13 16:14:51 +0300

    image_compress png8

--- actions/image_compress.sh
+++ actions/image_compress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-path=$1
-path=${path%/*}
+path=${1%/*}
+ext=${1##*.}
 
 if [[ -n `echo $path | grep "//"` ]]
 	then
@@ -10,10 +10,18 @@ if [[ -n `echo $path | grep "//"` ]]
 		exit
 fi
 
-parameters=`yad --borders=20 --width=500 --title="Сжатие изображений" \
-	--text-align=center --item-separator="|" --separator="," --form \
-	--field="Процент сжатия:SCL"	--field=" :LBL"		--field="Каталог для сохранения:DIR"	--field="Добавить суфикс к имени файла:CHK" \
-				"85"						""						"$path"									"FALSE"`
+if [ "$ext" == "png" ] || [ "$ext" == "PNG" ]
+	then
+		parameters=`yad --borders=20 --width=500 --title="Сжатие изображений PNG" \
+			--text-align=center --item-separator="|" --separator="," --form \
+			--field="Процент сжатия:SCL"	--field=" :LBL"		--field="Каталог для сохранения:DIR"	--field="Преобразовать в png8:CHK"	--field="Добавить суфикс к имени файла:CHK" \
+						"85"						""						"$path"										FALSE							"FALSE"`
+	else
+		parameters=`yad --borders=20 --width=500 --title="Сжатие изображений" \
+			--text-align=center --item-separator="|" --separator="," --form \
+			--field="Процент сжатия:SCL"	--field=" :LBL"		--field="Каталог для сохранения:DIR"				--field=" :LBL"				--field="Добавить суфикс к имени файла:CHK" \
+						"85"						""						"$path"																		"FALSE"`
+fi
 
 exit_status=$?
 
@@ -21,8 +29,15 @@ if [ $exit_status = 0 ]
 	then
 		quality=$( echo $parameters | awk -F ',' '{print $1}')
 		dir=$( echo $parameters | awk -F ',' '{print $3}')
-		sufix=$( echo $parameters | awk -F ',' '{print $4}')
-echo "dir=$dir"
+
+		png8=$( echo $parameters | awk -F ',' '{print $4}')
+		if [ $png8 == TRUE ]
+			then png8="png8:"
+			else png8=""
+		fi
+
+		sufix=$( echo $parameters | awk -F ',' '{print $5}')
+
 		kolfile=$#														# Количество выделенных файлов
 		procent=$((100/$kolfile))										# Процентов на каждый файл
 
@@ -34,7 +49,7 @@ echo "dir=$dir"
 					else file_out="$dir/$filename"
 				fi
 
-				convert "$file" -quality $quality "$file_out"
+				convert "$file" -quality $quality "$png8$file_out"
 
 				counter=$(($counter+1))
 				progress=$(($progress+$procent))
